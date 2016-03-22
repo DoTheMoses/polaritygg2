b38e61f64ea1e79de1387a8a2e006a3abd28af0d
DoTheMoses <matthewkiep@gmail.com>
2016-03-22 19:36:49 -0400

    Initial commit for testing

---
+++ functiontest.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<html>
+
+<!-- ================================================ -->
+<!-- begin head                                       -->
+<!-- ================================================ -->
+
+<head>
+
+<title>Polarity Website 2.0</title>
+<meta charset="utf-8" />
+<meta name="description" content="CFL-Based Tournament Source" /> 
+<meta name="keywords" content="Polarity, polarity, melee, ssbm, smash 4, sfv" />
+<meta name="author" content="Matt Kiep" />
+
+<?php require "page_functions.php"; ?>
+
+<?php pol_header(); ?>
+
+</head>
+
+<!-- ================================================ -->
+<!-- end head                                         -->
+<!-- ================================================ -->
+
+<!-- ================================================ -->
+<!-- begin body                                       -->
+<!-- ================================================ -->
+
+<body>
+
+<?php pol_navbar(); ?>
+
+<!-- ================================================ -->
+<!-- begin content                                    -->
+<!-- ================================================ -->
+
+<div class="container-fluid content-section">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+        <h1>Melee Tournaments & Events</h1>
+        <p>Polarity currently runs an ongoing weekly series in Orlando, Florida called CFL Smackdown.
+           It is held at Campus Cards & Games every Monday & Tuesday night, with Melee featured on Monday
+           nights. Turnout each week can reach upwards of 60-75 entrants, and it features some of the best 
+           players in the world.<br /><br />
+           
+           We also hold monthly CFL Smackdown tournaments in the same location. Entrants for Melee at these monthly
+           events can easily reach 100 or more people.<br /><br />
+           
+           Click <a href="events_calendar.html">here</a> to see our event calendar.<br /><br />
+           
+           To keep up with our events, follow us and subscribe to our events on <a href="index.html">Facebook</a>.
+        </p> 
+      </div>
+
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 hidden-xs">
+        <img class="img-responsive" src="images/hands.jpg" />
+      </div>
+
+    </div>           
+  </div>
+</div>
+
+<!-- ================================================ -->
+<!-- end content                                      -->
+<!-- ================================================ -->
+
+<?php pol_footer(); ?>
+
+</body>
+
+<!-- ================================================ -->
+<!-- end body                                         -->
+<!-- ================================================ -->
+
+</html>
