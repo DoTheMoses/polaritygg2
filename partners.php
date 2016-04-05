30d4717fe7bfd3de9ec6b275544ee35e800410a2
DoTheMoses <matthewkiep@gmail.com>
2016-04-05 12:32:38 -0400

    Update to php

---
+++ partners.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<html>
+
+<!-- =================================================================== -->
+<head>
+<!-- =================================================================== -->
+
+<head>
+
+<title>Polarity: Partners</title>
+<meta charset="utf-8" />
+<meta name="description" content="CFL-Based Tournament & Production Source" /> 
+<meta name="keywords" content="Polarity, polarity, melee, ssbm, smash 4, sfv" />
+<meta name="author" content="Matt Kiep" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="index.css" type="text/css" />
+
+<!-- =================================================================== -->
+</head>
+<!-- =================================================================== -->
+
+<!-- =================================================================== -->
+<body>
+<!-- =================================================================== -->
+
+<?php include("page_functions.php"); ?>
+
+<!-- ================================================ -->
+<!-- header and navbar                                -->
+<!-- ================================================ -->
+
+<?php get_pol_navbar(); ?>
+
+<!-- ================================================ -->
+<!-- content                                          -->
+<!-- ================================================ -->
+
+<!-- begin carousel -->
+<div id="Carousel" class="container-fluid">
+  <div class="row">
+
+    <div id="main_carousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="5000">
+      <!-- indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#main_carousel" data-slide-to="0" class="active"></li>
+        <li data-target="#main_carousel" data-slide-to="1"></li>
+        <li data-target="#main_carousel" data-slide-to="2"></li>
+      </ol>
+
+      <div class="carousel-inner" role="listbox"> 
+        
+        <!-- slide 0 -->
+        <div class="item active">
+          <img src="images/hands.jpg" alt="Grid">
+          <div class="carousel-caption-plus">
+            <div class="carousel-caption-plus-link">
+              <h1>Polarity Southeast Melee Invitational</h1>
+              <p>July 30th - 31st in Orlando, Florida</p>
+              <a href="https://www.facebook.com/events/1661446734125439/"></a>
+            </div>  
+          </div>
+        </div>
+
+        <!-- slide 1 -->
+        <div class="item">
+          <img src="images/plup.jpg" alt="Dudes">
+          <div class="carousel-caption-plus">
+            <div class="carousel-caption-plus-link">
+              <h1>The FBC Presents: Come to Papa</h1>
+              <p>March 19th-20th in Orlando, Florida</p>
+              <a href="https://smash.gg/tournament/the-fbc-presents-come-to-papa-1"></a>
+            </div>  
+          </div>
+        </div>
+
+        <!-- slide 2 -->
+        <div class="item">
+          <img src="images/hands.jpg" alt="Hands">
+          <div class="carousel-caption-plus">
+            <div class="carousel-caption-plus-link">
+              <h1>Impact 2016</h1>
+              <p>April 9th-10th in Orlando, Florida</p>
+              <a href="https://www.facebook.com/events/1661446734125439/"></a>
+            </div>  
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end carousel -->
+
+<!-- begin twtich -->
+<div class="container-fluid content-section">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+        <h1>Twitch</h1>
+        <p>Polarity's first major partnership began with Twitch. Our Twitch partnership allows us to 
+           better serve viewers watching our stream at home by providing coolio benefits.<br /><br />
+
+           Check out our Twitch channel here: <a href"https://www.twitch.tv/PolarityGG/">twitch.tv/PolarityGG</a>.
+           
+        </p> 
+      </div>
+
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 hidden-xs">
+        <img class="img-responsive" src="images/hands.jpg" />
+      </div>
+
+    </div>           
+  </div>
+</div>
+<!-- end twtich -->
+
+<!-- begin discord -->
+<div class="container-fluid content-section content-section-right">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 hidden-xs">
+        <img class="img-responsive" src="images/hands.jpg" />
+      </div>
+
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+        <h1>Discord</h1>
+        <p>Discord is also a partner. They provide ??
+        </p> 
+      </div>
+
+    </div>           
+  </div>
+</div>
+<!-- end discord -->
+
+<!-- begin partner with us -->
+<div class="container-fluid content-section" style="padding-top:30px; padding-bottom:30px">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <h1 style="text-align:center">Partner with Polarity</h1>
+        <p style="text-align:center">If you would like to discuss partnership options, please email us at partnership@polarity.gg.<br /><br />
+
+           We look forward to working with you!
+        </p> 
+      </div>
+
+    </div>           
+  </div>
+</div>
+<!-- end partner with us -->
+
+<!-- ========================================================= -->
+<!-- footer                                                    -->
+<!-- ========================================================= -->
+
+<?php get_pol_footer(); ?>
+
+<!-- =================================================================== -->
+</body>
+<!-- =================================================================== -->
+
+</html>
