a6e7afab7335124f0edcd8e34a4540e6367d06f6
DoTheMoses <matthewkiep@gmail.com>
2016-04-05 12:58:53 -0400

    Update to php

---
+++ events.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<html>
+
+<!-- =================================================================== -->
+<head>
+<!-- =================================================================== -->
+
+<title>Polarity: Events</title>
+<meta charset="utf-8" />
+<meta name="description" content="CFL-Based Tournament & Production Source" />
+<meta name="keywords" content="Polarity, polarity, melee, ssbm, smash 4, sfv" />
+<meta name="author" content="Matt Kiep" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="index.css" type="text/css" />
+
+<!-- =================================================================== -->
+</head>
+<!-- =================================================================== -->
+
+<!-- =================================================================== -->
+<body>
+<!-- =================================================================== -->
+
+<?php include("page_functions.php"); ?>
+
+<!-- ========================================================= -->
+<!-- header and navbar                                         -->
+<!-- ========================================================= -->
+
+<?php get_pol_navbar(); ?>
+
+<!-- ========================================================= -->
+<!-- content                                                   -->
+<!-- ========================================================= -->
+
+<div class="container-fluid">
+  <div class="container">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div style="text-align: center">
+          <img src="events-header.png" />
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="container-fluid">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23BCBCBC&amp;src=polaritygg%40gmail.com&amp;color=%23930000&amp;ctz=America%2FNew_York" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    </div>
+  </div>
+</div>
+
+<!-- ========================================================= -->
+<!-- footer                                                    -->
+<!-- ========================================================= -->
+
+<?php get_pol_footer(); ?>
+
+<!-- =================================================================== -->
+</body>
+<!-- =================================================================== -->
+
+</html>
