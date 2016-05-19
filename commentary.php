99a3c93b530d2ee00181bab1eb4207a4898ee219
DoTheMoses <matthewkiep@gmail.com>
2016-05-19 12:12:46 -0400

    Created the sections
    Created and added content to each section. Still needs a bit of cleanup.

--- commentary.php
+++ commentary.php
@@ -54,21 +54,21 @@
   <div class="container">
 
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding">
-      <div class="content-box">
+      <div class="content-box back-to-top">
         <img class="img-responsive" src="images/games/melee.png" alt="Super Smash Brothers Melee Logo" style="margin: auto; max-height: 130px;" />
 
         <div class="container-fluid no-padding">
           <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
-            <h2><a href="#tournaments_events">Tournaments & Events</a></h2>
+            <h2><a href="#how">How Can I Commentate?</a></h2>
           </div>
           <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
-            <h2><a href="#communities_resources">Communities & Resources</a></h2>
+            <h2><a href="#pacs">Polarity Approved Commentators</a></h2>
           </div>
           <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
-            <h2><a href="#players_rankings_results">Players, Rankings & Results</a></h2>
+            <h2><a href="#rules">Commentary Rules</a></h2>
           </div>
           <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
-            <h2><a href="#commentary">Commentary</a></h2>
+            <h2><a href="#resources">Resources</a></h2>
           </div>
         </div>
 
@@ -79,27 +79,142 @@
 </div>
 <!-- end top -->
 
-<!-- begin commentary -->
-<span class="anchor" id="commentary"></span>
+<!-- begin how -->
+<span class="anchor" id="how"></span>
 
 <div class="container-fluid content-section">
   <div class="container">
 
     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding hidden-xs">
-      <div class="content-box">
+      <div class="content-box content-box-image">
+        <img class="img-responsive" src="images/ruler.jpg" alt="Hands" />
+      </div>
+    </div>        
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding">
+      <div class="content-box back-to-top">          
+        <h3>How Can I Commentate?</h3>
+        <p>We've got a few things to consider!</p>
+
+        <h4>Commentary at CFL Smackdown (Weeklies)</h4>
+        <p>If you'd like to commentate at </p>
+        
+        <h4>Commentary at Larger Events</h4>
+        <p>For larger events, we'll post a separate form for you to fill out so we can make a 
+        proper commentator schedule prior to the event. These forms will be posted in the
+        <a href="https://www.facebook.com/groups/PolarityCommentators">Polarity Commentators 
+        Facebook group</a> so be sure to join the group to stay in the loop.</p>
+
+        <p><a href="#">Back to top</a></p>
+      </div>
+    </div>
+          
+  </div>
+</div>
+<!-- end how -->
+
+<!-- begin pacs -->
+<span class="anchor" id="pacs"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding hidden-xs">
+      <div class="content-box content-box-image">
+        <img class="img-responsive" src="images/ruler.jpg" alt="Hands" />
+      </div>
+    </div>        
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding">
+      <div class="content-box back-to-top">          
+        <h3>Polarity Approved Commentators</h3>
+        <p>Our commentators are some of the best around, and our commentary community has a lot going on. 
+        Our weekly CFL Smackdown series keeps things light, but for larger events we require prior approval.</p>
+
+        <p><a href="google.com">Click here to apply to be a Polarity Approved Commentator</a></p>
+
+        <h4>Benefits of being a Polarity Approved Commentators</h4>
+        <ul>
+          <li><b>You can commentate anytime</b>: If there's an open spot or you want to be scheduled, </li>
+          <li><b>We can schedule you</b>: If you decline to enter the weekly main event, we'll put you on schedule for a block</li>
+          <li><b>We can pay you!</b>If you are scheduled to commentate a weekly</li>
+        </ul>
+
+        <h4>List of Polarity Approved Commentators:</h4>
+        
+        <style>
+          .111 { }
+        
+        </style>
+        <ul>
+          <li><span class="111">&squf; = Smash 64, &squf; = Melee, &squf; = Smash 4</li>
+          <li><span class="010">&squf; &squf; &squf; </span>BiggPappa <a href="https://www.twitter.com/c0llinmari0tti/">@c0llinmari0tti</a></li>
+          <li><span class="010">&squf; &squf; &squf; </span>Dr4gonblitz <a href="https://www.twitter.com/dr4gonBlitz/">@dr4gonBlitz</a></li>
+          <li><span class="001">&squf; &squf; &squf; </span>EE | Duelist <a href="https://www.twitter.com/EE_Duelist/">@EE_Duelist</a></li>
+          <li><span class="010">&squf; &squf; &squf; </span>Oaty <a href="https://www.twitter.com/ProbablyOaty/">@ProbablyOaty</a></li>
+          <li><span class="101">&squf; &squf; &squf; </span>Rematch <a href="https://www.twitter.com/WannaRematch/">@WannaRematch</a></li>
+          <li><span class="010">&squf; &squf; &squf; </span>Steele <!-- <a href="https://www.twitter.com/?/">?</a> --></li>
+          <li><span class="011">&squf; &squf; &squf; </span>OHS | The Toolbag <a href="https://www.twitter.com/the_toolbag/">@the_toolbag</a></li>          
+        </ul>
+        
+        <p><a href="#">Back to top</a></p>
+      </div>
+    </div>
+          
+  </div>
+</div>
+<!-- end pacs -->
+
+<!-- begin rules -->
+<span class="anchor" id="rules"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding hidden-xs">
+      <div class="content-box content-box-image">
+        <img class="img-responsive" src="images/ruler.jpg" alt="Hands" />
+        <p>Caption</p>
+      </div>
+    </div>        
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding">
+      <div class="content-box back-to-top">          
+        <h3>Commentary Rules</h3>
+        <p>For any event we produce, we want to ensure both quality and entertainment for all audiences, 
+        and our list of commentary rules and guidelines was tailored to reflect that. If you apply
+        to become a <b>Polarity Approved Commentator</b>, you will have to understand and agree to abide by
+        the rules.</p>
+
+        <p><a href="https://www.polarity.gg/CommentaryRules.pdf">You can find the rules here.</a></p>
+
+        <p><a href="#">Back to top</a></p>
+      </div>
+    </div>
+          
+  </div>
+</div>
+<!-- end rules -->
+
+<!-- begin resources -->
+<span class="anchor" id="resources"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding hidden-xs">
+      <div class="content-box content-box-image">
         <img class="img-responsive" src="images/ruler.jpg" alt="Hands" />
       </div>
     </div>        
 
     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding">
       <div class="content-box">          
-        <h3>Commentary</h3>
-        <p>Our commentators are some of the best around, and our commentary community has a lot going on. 
-        Our weekly CFL Smackdown series keeps things light, but for larger events we require prior approval.</p>
+        <h3>Resources</h3>
+        <p>Even your favorite commentators had to start somewhere. Below is a list of links to help
+        improve your commentary, whatever stage you're at on your path to greatness.</p>
 
-        <p>Want to give it a shot on the mic? Click here to fill out our <a href="google.com">Commentary Form</a>.</p>
-
-        <h4>List of our approved melee commentators:</h4>
+        <h4>Useful Links</h4>
         <ul>
           <li>Marsfool <a href="https://www.twitter.com/20XXLIVINGROOM/">@20XXLIVINGROOM</a></li>
           <li>Dr4gonblitz <a href="https://www.twitter.com/Dr4gonblitz/">@Dr4gonBlitz</a></li>
@@ -112,7 +227,7 @@
           
   </div>
 </div>
-<!-- end commentary -->
+<!-- end resources -->
 
 </content>
 
