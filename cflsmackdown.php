eee98a02866bfd11968efe6e9c440902f35d2020
DoTheMoses <matthewkiep@gmail.com>
2016-06-01 09:56:46 -0400

    Added pages
    CFL Smackdown and Internal pages added

---
+++ cflsmackdown.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  
+<!-- =================================================================== -->
+<head>
+<!-- =================================================================== -->
+
+<title>Polarity: CFL Smackdown</title>
+<meta charset="utf-8" />
+<meta name="description" content="CFL-Based Tournament & Production Source" />
+<meta name="keywords" content="Polarity, polarity, melee, ssbm, smash 4, sfv" />
+<meta name="author" content="Matt Kiep" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<link href='https://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css' />
+<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css' />
+
+<link rel="stylesheet" href="index.css" type="text/css" />
+<script type="text/javascript" src="index.js"></script>
+
+<link rel="shortcut icon" href="images/fav_icon.png" type="image/x-icon" />
+
+<!-- =================================================================== -->
+</head>
+<!-- =================================================================== -->
+
+<!-- =================================================================== -->
+<body>
+<!-- =================================================================== -->
+
+<?php include("page_functions.php"); ?>
+
+<!-- ========================================================= -->
+<header>
+<!-- ========================================================= -->
+
+<?php get_pol_navbar(); ?>
+
+</header>
+
+<!-- ========================================================= -->
+<content>
+<!-- ========================================================= -->
+
+<!-- begin top-header -->
+<div class="container-fluid content-section-top top-games">
+  <div class="container">
+
+    <div class="container-fluid content-section-top-title">
+      <img class="img-responsive" src="images/cfl_smackdown_logo.png" alt="CFL Smackdown Logo" />
+    </div>
+
+  </div>
+</div>
+<!-- end top-header -->
+
+<!-- begin top-content -->
+<div class="container-fluid content-section">
+  <div class="container">
+
+    <div class="content-box">
+      <p>CFL Smackdown is the longest running and most well-known Smash weekly in Florida.</p>
+
+      <p>Click on the links below to find out everything you need to know about playing at 
+      CFL Smackdown.</p>      
+
+      <div class="container-fluid no-padding">
+        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
+          <h2><a href="#Melee">Melee</a></h2>
+        </div>
+        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
+          <h2><a href="#Fees">Fees</a></h2>
+        </div>
+        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
+          <h2><a href="#64">Smash 64</a></h2>
+        </div>
+        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
+          <h2><a href="#PM">Project: Melee</a></h2>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</div>
+<!-- end top-content -->
+
+<!-- begin Melee -->
+<span class="anchor" id="Melee"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+      
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding">
+      <div class="content-box back-to-top">
+        <h3>Melee</h3>
+        <h4>Fees</h4>
+        <ul>        
+          <li>Venue Fee: $5</li>
+          <li>Singles Entry Fee: $3</li>          
+          <li>Doubles Entry Fee: $4 per team ($2 per person)</li>
+          <li>Entry Fee for other events will be noted on event page</li>          
+        </ul>
+
+        <h4>Payouts</h4>
+        <h4>N < 64</h4>
+        <ul>
+          <li>1st: 60%</li>
+          <li>2nd: 30%</li>
+          <li>3rd: 10%</li>
+        </ul>
+        <h4>N =< 64</h4>
+        <ul>
+          <li>1st: 55%</li>
+          <li>2nd: 30%</li>
+          <li>3rd: 10%</li>
+          <li>4th: 5%</li>
+        </ul>
+      
+        <h4><b>General Rules</b></h4>
+        <ul>
+          <li>Items off, 8 Minutes, 4 Stocks</li>
+          <li>All non-finals sets are best 2 of 3</li>
+          <li>Winner's Finals, Loser's Finals, and Grand Finals are besst 3 of 5</li> 
+          <li>No excessive stalling (Rising Pound, Luigi ladder, etc.)</li>
+          <li>Wobbling is <b>LEGAL</b> until 300%, at which point it is stalling</li>
+          <li>Double Blind and Neutral Start can be requested</li>
+          <li>DSR (Dave' Stupid Rule): You cannot counter pick to any stage you have won on that set</li>
+          <li>Gentleman’s Clause: Any stage may be played on if both players agree to it.</li>
+          <li>In case of match time out, the winner is determined first by stocks, then lowest percentage</li>
+          <li>Pausing results in a <b>stock loss</b>, at the discretion of the opponent</li>
+          <li>Winning player not locked into same character for the next game, but has to pick before loser selects character</li>
+          <li>Starting stage is decided by striking from the Starter Stages in P1-P2-P2-P1 order</li>
+          <li>Rock, paper, scissors is the generally accepted way to determine who strikes first in stage striking</li>
+          <li>Stages in following games decided by loser after winner bans 1 stage</li>
+          <li>Best 3 of 5 sets have no bans</li>
+        </ul>
+
+        <h4>Singles Stage List</h4>
+        <h4>Starters/Neutrals</h4>
+        <ul>
+          <li>Yoshi’s Story</li>
+          <li>Fountain of Dreams</li>
+          <li>Battlefield</li>
+          <li>Final Destination</li>
+          <li>Dream Land</li>
+        </ul>
+        <h4>Counterpicks</h4>
+        <ul>        
+          <li>Pokemon Stadium</li>
+        </ul>
+
+        <h4>Doubles Stage List</h4>
+        <h4>Starters/Neutrals</h4>
+        <ul>
+          <li>Yoshi’s Story</li>
+          <li>Pokemon Stadium</li>
+          <li>Battlefield</li>
+          <li>Final Destination</li>
+          <li>Dream Land</li>
+        </ul>
+        <h4>Counterpicks</h4>
+        <ul>        
+          <li>None</li>
+        </ul>  
+
+        <p><a href="#">Back to top</a></p> 
+      </div>
+    </div>
+      
+  </div>
+</div>
+<!-- end Melee -->
+
+<!-- begin WiiU -->
+<span class="anchor" id="Fees"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+      
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding">
+      <div class="content-box back-to-top">
+        <h3>Fees & Payouts</h3>
+        <p>Venue Fees are<p>
+
+        <h4>Payouts</h4>
+        <h5>N < 16</h5>
+        <ul>
+          <li>1st: 60%</li>
+          <li>2nd: 30%</li>
+          <li>3rd: 10%</li>
+        </ul>
+
+        <p><a href="#top">Back to top</a></p>
+      </div>
+    </div>
+    
+  </div>
+</div>
+<!-- end WiiU -->
+
+<!-- begin 64 -->
+<span class="anchor" id="64"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+      
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding">
+      <div class="content-box back-to-top">
+        <h3>Super Smash Bros (64)</h3>
+        <p>What is z-cancelling? Where I can find other Smash 64 players? You can play Smash 64 online?!
+        Answers to all of these questions and more are found in the links below.</p>
+
+        <ul>
+          <li><a href="https://www.facebook.com/groups/1613096295628159">Florida Smash 64 Facebook Group</a> - CFL Smash 64 Community</li>
+          <li><a href="https://www.facebook.com/groups/USAssb64">NA SSB64 Community</a> - Facebook Group for North American Smash 64</li>
+          <li><a href="https://www.facebook.com/groups/329401377087352/">SSB64 Online</a> - Facebook Group for Online Smash 64</li>
+          <li><a href="https://www.facebook.com/groups/134497873392355/">Worldwide SSB64</a> - Worldwide Facebook Group for Smash 64</li>
+          <li><a href="https://smashboards.com/">Smashboards</a> - The original source for everything Smash</li>
+          <li><a href="https://www.reddit.com/r/ssb64/">/r/ssb64</a> - Subreddit specifically about Smash 64</li>
+          <li><a href="https://www.reddit.com/r/smashbros/">/r/smashbros</a> - Subreddit with all Smash games showcased</li>
+        </ul>
+
+        <p><a href="#top">Back to top</a></p>
+      </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding">
+      <div class="content-box content-box-image">
+        <img class="img-responsive" src="images/pages/games/wizzy.jpg" alt="Wizzrobe plays Smash 64 at Frame One" />
+        <p>Some say his opponents are still in hitstun.</p>
+      </div>
+    </div>
+      
+  </div>
+</div>
+<!-- end 64 -->
+
+<!-- begin PM -->
+<span class="anchor" id="PM"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+      
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding">
+      <div class="content-box content-box-image">
+        <img class="img-responsive" src="images/pages/games/lz.jpg" alt="Mr Lz plays at Frame One" />
+        <p>The "Mr" in Mr Lz's gamterag isn't fooling anyone.</p>
+      </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding">
+      <div class="content-box back-to-top">
+        <h3>Project: Melee</h3>
+        <p>What is z-cancelling? Where I can find other Smash 64 players? You can play Smash 64 online?!
+        Answers to all of these questions and more are found in the links below.</p>
+
+        <ul>
+          <li><a href="https://www.facebook.com/groups/1613096295628159">Florida Smash 64 Facebook Group</a> - CFL Smash 64 Community</li>
+          <li><a href="https://www.facebook.com/groups/USAssb64">NA SSB64 Community</a> - Facebook Group for North American Smash 64</li>
+          <li><a href="https://www.facebook.com/groups/329401377087352/">SSB64 Online</a> - Facebook Group for Online Smash 64</li>
+          <li><a href="https://www.facebook.com/groups/134497873392355/">Worldwide SSB64</a> - Worldwide Facebook Group for Smash 64</li>
+          <li><a href="https://smashboards.com/">Smashboards</a> - The original source for everything Smash</li>
+          <li><a href="https://www.reddit.com/r/ssb64/">/r/ssb64</a> - Subreddit specifically about Smash 64</li>
+          <li><a href="https://www.reddit.com/r/smashbros/">/r/smashbros</a> - Subreddit with all Smash games showcased</li>
+        </ul>
+
+        <p><a href="#top">Back to top</a></p>
+      </div>
+    </div>
+      
+  </div>
+</div>
+<!-- end PM -->
+
+</content>
+
+<!-- ========================================================= -->
+<!-- footer                                                    -->
+<!-- ========================================================= -->
+
+<?php get_pol_footer(); ?>
+
+<!-- =================================================================== -->
+</body>
+<!-- =================================================================== -->
+
+</html>
---
+++ int.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<!-- =================================================================== -->
+<head>
+<!-- =================================================================== -->
+
+<title>Polarity: Internal Page</title>
+<meta charset="utf-8" />
+<meta name="description" content="CFL-Based Tournament & Production Source" />
+<meta name="keywords" content="Polarity, polarity, melee, ssbm, smash 4, sfv" />
+<meta name="author" content="Matt Kiep" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<link href='https://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css' />
+<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css' />
+
+<link rel="stylesheet" href="index.css" type="text/css" />
+<script type="text/javascript" src="index.js"></script>
+
+<link rel="shortcut icon" href="images/fav_icon.png" type="image/x-icon" />
+
+<!-- =================================================================== -->
+</head>
+<!-- =================================================================== -->
+
+<!-- =================================================================== -->
+<body>
+<!-- =================================================================== -->
+
+<?php include("page_functions.php"); ?>
+
+<!-- ========================================================= -->
+<header>
+<!-- ========================================================= -->
+
+<?php get_pol_navbar(); ?>
+
+</header>
+
+<!-- ========================================================= -->
+<content>
+<!-- ========================================================= -->
+
+<!-- begin top-header -->
+<div class="container-fluid content-section-top">
+  <div class="container">
+
+    <div class="container-fluid content-section-top-title">
+      <h1>Internal Stuff</h1>
+    </div>
+
+  </div>
+</div>
+<!-- end top-header -->
+
+<!-- begin top-content -->
+<div class="container-fluid content-section">
+  <div class="container">
+
+    <div class="content-box">
+      <p>Here's all our internal stuff.</p>
+
+      <div class="container-fluid no-padding">
+        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
+            <h2><a href="#Backet_Upload">Bracket Upload</a></h2>
+        </div>        
+        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
+            <h2><a href="#Announcements">Announcements</a></h2>
+        </div>
+        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
+            <h2><a href="#Season_2">Season 2</a></h2>
+            <p>November 14th, 2015 - February 6th, 2016</p>
+        </div>
+        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
+            <h2><a href="#Season_3">Season 3</a></h2>
+            <p>Coming Soon!</p>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</div>
+<!-- end top-content -->
+
+<!-- begin Backet_Upload -->
+<span class="anchor" id="Backet_Upload"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+
+    <div class="container-fluid content-box back-to-top">
+      <h3>Backet Upload</h3>
+
+      <p>First go to <a href="https://t.polarity.gg/admin/">https://t.polarity.gg/admin/</a>.
+      In the field in the middle of the page, type the usual password, with the first two letters
+      capitalized, and a ! at the end.
+      </p>
+
+      <p><a href="#">Back to top</a></p>
+    </div>
+
+  </div>
+</div>
+<!-- end Backet_Upload -->
+
+<!-- begin Announcements -->
+<span class="anchor" id="Announcements"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+
+    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7 no-padding">
+      <div class="content-box back-to-top">
+        <h3>Announcements</h3>
+
+        <p> Here are our announcements for today:</p>
+
+        <ul>
+          <li>Tits</li>
+          <li>Balls</li>
+        </ul>
+
+        <p><a href="#">Back to top</a></p> 
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- end Announcements -->
+
+<!-- begin Season_2 -->
+<span class="anchor" id="Season_2"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+
+    <div class="col-xs-12 col-sm-5 col-sm-5 col-lg-5 no-padding">
+      <div class="content-box content-box-image">
+        <img class="img-responsive" src="images/pages/aml/oaty_vs_mike.jpg" alt="Oaty plays Mike on AML Season 2 Finals Day"/>
+        <p>Oaty and OC Mike struggle mightily to contain the immense amount of hype they feel inside
+        during Season 2 Finals Day.</p>
+      </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7 no-padding">
+      <div class="content-box back-to-top">
+        <h3>Season 2</h3>
+        <h5>November 14th, 2015 - February 6th, 2016</h5>
+        <ul>
+          <li><a href="http://smashboards.com/threads/the-amateur-melee-league-season-2-cfl.422603/">Rules</a> &squf;</li>
+          <li><a href="https://docs.google.com/spreadsheets/d/1SSOA6QJm5pG-0t3xZpVmeQCWhNfnXSmIgRA7VFvA0fs/edit?usp=sharing/">Final ELO Standings</a> &squf;</li>
+          <li><a href="https://docs.google.com/spreadsheets/d/1s5AfB0umxPIrM0C8dLjQ5ZIGXqF04Pzh2guqNJ8CiKs/edit?usp=sharing/">Match Histories</a> &squf;</li>
+          <li><a href="https://challonge.com/amlseason2finals/">Final Bracket</a> &squf;</li>
+          <li><a href="https://www.youtube.com/playlist?list=PLWqagSqx492eSc7czLhAOKBlSgnY01kmA">VODs</a></li>
+        </ul>
+
+        <p>Season 2 was even better than Season 1. Fearless came up with a brilliant
+        idea to get rid of pools, and instead use an Elo ranking system. He wrote custom
+        software to handle this new system, and the players loved it. Find out more about
+        the system at the <b>Rules</b> link above.</p>
+
+        <p>Season 2 Finals day was held at the CFL Smackdown February Monthly. We were able 
+        to get way more matches on stream (found in the VODs link above), and plenty of the 
+        matches went to three games, which indicated to us that the seeding from the season's 
+        final results was pretty close to ideal. There was an outlier, however. Mr Lz finished 
+        the season in 9th place, but ended up taking a pretty clean ride to Grand Finals. 
+        There, he took only 10 minutes to 3-0 OC Mike to become the Season 2 Champion.</p>
+
+        <h4>Season 2 Top 8</h4>
+        <ul>
+          <li>1. <b>Mr Lz</b> <img src="/2/images/ssbm_icons/MarthHeadSSBM.png" alt="Melee Icon - Marth" />, 
+          Lz stands for Look the Fuck Out Peoplez, $165</li>
+          <li>2. <b>OC Mike</b> <img src="/2/images/ssbm_icons/FoxHeadSSBM.png" alt="Melee Icon - Fox" />, 
+          the Cold Stone Killer, $84</li>
+          <li>3. <b>Oaty</b> <img src="/2/images/ssbm_icons/PeachHeadSSBM.png" alt="Melee Icon - Peach" />, 
+          the Floaty Goaty like Whoa-ty, $62</li>
+          <li>4. <b>Lad</b> <img src="/2/images/ssbm_icons/FoxHeadSSBM.png" alt="Melee Icon - Fox" />, 
+          Hype Incarnate, $41</li>
+          <li>5. <b>Ridz</b> <img src="/2/images/ssbm_icons/FoxHeadSSBM.png" alt="Melee Icon - Fox" /> / 
+          <img src="/2/images/ssbm_icons/falco.png" alt="Melee Icon - Falco" />, 
+          Sloth Inside, Lightning Speed Space Animal Outside, $21</li>
+          <li>5. <b>G0D</b> <img src="/2/images/ssbm_icons/CaptainFalconHeadSSBM.png" alt="Melee Icon - Captain Falcon" />, 
+          (insert badass falcon screech as it divebombs its prey here), $21</li>
+          <li>7. <b>Envy</b> <img src="/2/images/ssbm_icons/JigglypuffHeadSSBM.png" alt="Melee Icon - Jigglypuff" />, 
+          Who Needs a Nickname When Your Actual Name is Matt Fucking Powers?, $10</li>
+          <li>7. <b>Kaike</b> <img src="/2/images/ssbm_icons/FalcoHeadSSBM.png" alt="Melee Icon - Falco" />, 
+          Son of Father and Father of Falco Beatdowns, $10</li>
+        </ul>  
+
+        <p><a href="#">Back to top</a></p> 
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- end Season_2 -->
+
+<!-- begin Season_3 -->
+<span class="anchor" id="Season_3"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding">
+      <div class="content-box back-to-top">
+        <h3>Season 3</h3>
+        <h5>?? - ??</h5>
+
+        <p class="centered">Season 3 is in the works!</p>
+
+        <p class="centered">Click the button below to register for Season 3!</p>
+
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick">
+        <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHXwYJKoZIhvcNAQcEoIIHUDCCB0wCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBdx2O5nlB2NuYEENz11zU0/YwGjLpJp2KyIgbbJv5EcCJwAUL/YYKop+E/OpuFvWd7K667XQ4ZsWiDYaey7aO9Lp/nezM41raMDi7JmDeC+A5Ow+V5AMPWA4ukaXJK+3xNM1+sVSl3RwUwLKWQGbUwbzBsT2jnUs/7ePc78jfIDTELMAkGBSsOAwIaBQAwgdwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQI4uh6GICBXZmAgbh3Xoj7jxIoKB4Mx/JxTL5e2m+91RgtqVlVSWZiWzAmL5HmM7Ux9/y0FqeVFL+xm7ACI45VRm28j/8SOXWAD5VNakQfn1cuYJ43UoRdcQ/k5OY6lGrvua6YQYZDo/ic7SRJ028ywc0ZCti2xUwyOm06dMiRvXVrysbHATi3dl91XXZ6Z36sW+a5VLtP6u7eq/oU26bYnSBbqhkJdY5DwkVMlA6vZQsT7AuaY521gUYGOIsbc93zY2cyoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTYwNTIyMDQxODU3WjAjBgkqhkiG9w0BCQQxFgQUjvGGl+Cu4Ofh+ADB8SUs82shwrcwDQYJKoZIhvcNAQEBBQAEgYCabybBrfOYwKAOGg1n5l4M4qLFLFXpZLcbzKYS+5ko0kAd3tVxydw/wE8ZUdKJ9FoDFkXa+04tZrkb+5t8pl757EOGcQtOFOSJ1bh+oXHqp3LuemMBHavzMrkeNf2O50EGvbocCxGokDkcEljmP4qgL2fVj4unQKcXDZu8ztmEIg==-----END PKCS7-----
+        ">
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>
+
+        <p><a href="#">Back to top</a></p> 
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- end Season_3 -->
+
+</content>
+
+<!-- ========================================================= -->
+<!-- footer                                                    -->
+<!-- ========================================================= -->
+
+<?php get_pol_footer(); ?>
+
+<!-- =================================================================== -->
+</body>
+<!-- =================================================================== -->
+
+</html>
