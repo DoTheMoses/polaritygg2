9dd86987ac82908c5ef1bd08a46763439a0246e7
DoTheMoses <matthewkiep@gmail.com>
2016-06-01 15:19:32 -0400

    Update and rename contact.php to connect.php
    Combined the Contact and Partners pages

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@
 <head>
 <!-- =================================================================== -->
 
-<title>Polarity: Contact Us</title>
+<title>Polarity: Connect With Us</title>
 <meta charset="utf-8" />
 <meta name="description" content="CFL-Based Tournament & Production Source" /> 
 <meta name="keywords" content="Polarity, polarity, melee, ssbm, smash 4, sfv" />
@@ -56,7 +56,7 @@
   <div class="container">
 
     <div class="container-fluid content-section-top-title">
-      <h1>Contact</h1>
+      <h1>Connect</h1>
     </div>
 
   </div>
@@ -79,7 +79,8 @@
           <p>If you'd like to talk to us about a partnership opportunity, contact
           <a href="mailto:admin@polarity.gg?Subject=Partnership%20Opportunity" target="_top">
           admin@polarity.gg</a>.
-          </p>
+          Our current major partners are <a href="#Twitch">Twitch</a> and 
+          <a href="#Discord"></a>.</p>
         </div>
 
         <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 no-padding">
@@ -111,25 +112,81 @@
 </div>
 <!-- end top-content -->
 
-<!-- begin top -->
-<div class="container-fluid content-section-top">
+<!-- begin Twitch -->
+<span class="anchor" id="Twitch"></span>
+
+<div class="container-fluid content-section">
   <div class="container">
 
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding">
-      <div class="container content-section-top-title">
-        <h1>Contact Us</h1>
-      </div> 
+      <div class="content-box back-to-top">
+        <h3><img src="images/Twitch_logo_black.svg" style="max-height: 150px;" alt="Twitch.tv"/></h3>
+        <p><i>"Founded in June 2011, Twitch is the world’s leading social video platform and community for gamers. 
+        Each month, more than 100 million community members gather to watch and talk about video games with 
+        more than 1.7 million broadcasters. Twitch’s live and video on demand platform caters to the entire 
+        video game industry, including game developers, publishers, media outlets, events, casual content 
+        creators, and the entire esports scene."</i> - <a href="https://www.twitch.tv/p/about/">Source</a><p>
 
-      <div class="content-section-top-text">
+        <p>Polarity's first major partnership began in early 2016 with <a href="https://www.twitch.tv/">Twitch</a>. 
+        Our Twitch partnership allows us to better serve viewers watching our stream and enables our viewers to 
+        directly support Polarity through subscriptions.</p>
+        
+        <p>Shoutouts to <a href="https://www.twitter.com/atrioc/">Atrioc at Twitch</a> who helped us become
+        partnered and helped us get Twitch front page promotion for months in early 2016.</p>
 
+        <p><a href="https://www.twitch.tv/PolarityGG/">Check out our Twitch channel here.</a>
+        If you like what you see, Follow the channel! If you love what you see, Subcribe!</p> 
 
-      </div> <!-- content-section-top-text -->
+        <h4>Subcriber Benefits</h4>
+        <ul>
+          <li>Dope Sub badge in Twitch chat</li>
+          <li>Sweet Twtich Emotes<img src="images/twitch_emotes/polFlako.png" alt="polFlako" /> &squf;
+            <img src="images/twitch_emotes/polPappa.png" alt="polPappa" /> &squf;
+            <img src="images/twitch_emotes/polBruh.png" alt="polBruh" /> &squf;
+            <img src="images/twitch_emotes/polPlup.png" alt="polPlup" /></li>
+          <li>Use of Polarity discord App</li>
+          <li>Free venue fee on designated Sub Nights</li>
+         </ul>
+    
+        </p> 
 
-    </div> <!-- cols -->
+       <p><a href="#top">Back to top</a></p>
+      </div>
+    </div>
 
   </div>
 </div>
-<!-- end top -->
+<!-- end Twitch -->
+
+<!-- begin discord -->
+<span class="anchor" id="Discord"></span>
+
+<div class="container-fluid content-section">
+  <div class="container">
+
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding">
+      <div class="content-box back-to-top">
+        <h3><img src="images/Discord-Logo.png" style="max-height: 150px;" alt="Discord"/></h3>
+        <p><i>"We think it's time to ditch Skype and Teamspeak. Two hundred seventy million PC 
+        gamers use these apps to communicate while playing online games yet they haven't been 
+        updated in years and no longer meet our needs. As gamers ourselves, we got fed up with 
+        these tools and decided to fix the problem ourselves. As a result, we've built the 
+        best all-in-one voice and text chat app for gamers that's free, secure, and works on 
+        both desktop and phone. It's called Discord. Fortunately, a lot of people love it."</i> -  
+        <a href="https://discordapp.com/company/">Source</a></p>
+
+        <p>Polarity's second major partnership came with <a href="https://www.discord.com/">Discord</a>. 
+        Our Discord partnership allows us more efficient and better use of Discord as a tool. We use discord
+        internally for all our online meetings and also use our discord channel as a place for our Twitch
+        Subscibers to gather and chat.</p>
+
+        <p><a href="#top">Back to top</a></p>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- end discord -->
 
 </content>
 
