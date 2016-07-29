b250bbf10f4c7832ac7e579c3a9aae7316fd3433
DoTheMoses <matthewkiep@gmail.com>
2016-07-29 15:44:00 -0400

    Initial the Smash House Work

---
+++ thesmashhouse.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  
+<!-- =================================================================== -->
+<head>
+<!-- =================================================================== -->
+
+<title>Polarity: The Smash House</title>
+<meta charset="utf-8" />
+<meta name="description" content="CFL-Based Tournament & Production Source" />
+<meta name="keywords" content="Polarity, polarity, melee, ssbm, smash 4, sfv" />
+
+<?php include("page_functions.php"); ?>
+
+<?php get_pol_head_section(); ?> 
+
+<!-- ========================================================= -->
+<!-- content                                                   -->
+<!-- ========================================================= -->
+
+<div class="animated-bg">
+  <div class="sh-top-logo">
+    <img src="https://www.polarity.gg/2/images/thesmashhouse.png" alt="The Smash House Logo">
+  </div>
+</div>
+
+<!-- begin Players -->
+<span class="anchor" id="Players"></span>
+<div class="container-fluid sh-section">
+  <div class="container-fluid sh-section-inner">
+
+    <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+      <div class="container-fluid sh-section-header sh-section-header-left">
+        <h1>Players</h1>
+      </div>
+    </div>
+
+      <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+        <div class="container-fluid">
+          <p>Players</p>
+        </div>
+      </div>  
+
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <div class="container-fluid sh-section-content sh-section-content-left">
+        <p>Players are the best</p>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!-- =================================================================== -->
+</body>
+<!-- =================================================================== -->
+
+</html>
